4f4ec7dc85c16339f4829d25e582eef8519205e0
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2023-02-20 15:10:48 +0530

    Atlas Live

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/InvoiceDatabase";
+// const mongoURI = "mongodb://localhost:27017/InvoiceDatabase";
+const mongoURI = "mongodb+srv://bhanu1:QDpKVzYX9yNUt12Q@cluster0.acl8nx3.mongodb.net/Iv-Maker?retryWrites=true&w=majority";
 
 mongoose.set('strictQuery', false);
 const connectToMongo = () => {
