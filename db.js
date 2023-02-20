081176af555a60fbf5bc5cd0d885d02bf2749fa7
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2023-02-20 11:54:19 +0530

    strictQuery false

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const mongoURI = "mongodb://localhost:27017/InvoiceDatabase";
 
+mongoose.set('strictQuery', false);
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("connected To Mongo Succesfully");
