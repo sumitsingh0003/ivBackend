e3ff6a2c25eb0b81507fb9b12cbab0bfac1c2f09
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2023-02-20 13:41:51 +0530

    database

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mongoURI = "mongodb://localhost:27017/InvoiceDatabase";
 
 mongoose.set('strictQuery', false);
 const connectToMongo = () => {
-  mongoose.connect(mongoURI, () => {
+  mongoose.connect(mongoURI).then(() => {
     console.log("connected To Mongo Succesfully");
-  });
+  }).catch((e) => console.log(e));
 };
 
 module.exports = connectToMongo;
