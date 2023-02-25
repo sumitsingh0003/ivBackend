c2c337bf0d0f018b0bd6f1bf782f3dcc4cdf2450
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2023-02-25 22:57:29 +0530

    Working

--- routes/notes.js
+++ routes/notes.js
@@ -97,38 +97,65 @@ router.get("/fetchnotes", fetchuser, async (req, res) => {
 
 // Using put - Because when we update an existing thing we use put
 // Route 3 : Update an existing Note using - Put "/api/notes/updatenote" . Login Required
-router.put("/updatenote/:id", fetchuser, async (req, res) => {
+router.put("/updatenote/:id", upload.single('Info[img]'), fetchuser, async (req, res) => {
   try {
-    const { data } = req.body;
-    // Creating a newNote Object
-    const newNote = {};
-    if (data) {
-      newNote.data = data;
-    }
-    // if (description) {
-    //   newNote.description = description;
-    // }
-    // if (tag) {
-    //   newNote.tag = tag;
-    // }
+    // debugger;
+    const items = JSON.parse(req.body.items);
+    if (req.file?.filename) {
+      const { imgName, ...NewInfo } = req.body.Info;
+      const { subTotalData } = req.body;
+      const url = req.protocol + '://' + req.get('host');
+      const imgDataName = url + "/images/" + req.file.filename;
+      const Info = { ...NewInfo, imgDataName };
+      // return { subTotalData, Info };
 
-    // Find the note to be updated and update it
-    //  const note = Note.findByIdAndUpdate()
-    // Ye nhi karenge kyuki isme koi bhi kisika note change kar sakta hai kyuki isme user ki id or edit karne wale ki id ko humne verify nhi kiya hai ki wo dono ek hi bande hai ya nhi
+      // Creating a newNote Object
+      const newNote = {};
+      if (Info && items && subTotalData) {
+        newNote.data = { Info: Info, items: items, subTotalData: subTotalData };
+      }
+      // if (description) {
+      //   newNote.description = description;
+      // }
+      // if (tag) {
+      //   newNote.tag = tag;
+      // }
 
-    let note = await Note.findById(req.params.id);
-    if (!note) {
-      return res.status(404).send("Not Found");
-    }
-    //  note.user.toString() -- Us note ki id hai jo edit ho rha hai 
-    //  req.user.id -- edit karne wale ki id hai 
-    //  hum jab note ko add karte hai tab hum us note ko id dete hai or us note me uske user ki id bhi daalte hai
-    if (note.user.toString() !== req.user.id) {
-      return res.status(401).send("Not Allowed to update");
-    }
+      // Find the note to be updated and update it
+      //  const note = Note.findByIdAndUpdate()
+      // Ye nhi karenge kyuki isme koi bhi kisika note change kar sakta hai kyuki isme user ki id or edit karne wale ki id ko humne verify nhi kiya hai ki wo dono ek hi bande hai ya nhi
 
-    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
-    res.json({ note });
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("Not Found");
+      }
+      //  note.user.toString() -- Us note ki id hai jo edit ho rha hai 
+      //  req.user.id -- edit karne wale ki id hai 
+      //  hum jab note ko add karte hai tab hum us note ko id dete hai or us note me uske user ki id bhi daalte hai
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed to update");
+      }
+
+      note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+      res.json({ note });
+    } else {
+      const { Info, subTotalData } = req.body;
+      const newNote = {};
+      if (Info && items && subTotalData) {
+        newNote.data = { Info: Info, items: items, subTotalData: subTotalData };
+      }
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("Not Found");
+      }
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed to update");
+      }
+
+      note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+      res.json({ note });
+      // return { subTotalData, Info };
+    }
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Serval Error Occured");
@@ -234,3 +261,49 @@ module.exports = router;
 //     }
 //   }
 // );
+
+
+
+// const express = require('express');
+// const multer = require('multer');
+// const path = require('path');
+
+// const app = express();
+// const upload = multer({ dest: path.join(__dirname, 'uploads') });
+
+// app.put('/api/updateData/:id', upload.single('image'), async (req, res) => {
+//   const { id } = req.params;
+//   const { text, image } = req.body;
+
+//   try {
+//     const data = await Data.findById(id);
+//     if (!data) {
+//       return res.status(404).json({ message: 'Data not found' });
+//     }
+
+//     if (image) {
+//       // If a new image was uploaded, remove the previous image and update the image URL in the database
+//       if (data.imageUrl) {
+//         // Remove the previous image file from the server
+//         const imagePath = path.join(__dirname, 'public', data.imageUrl);
+//         fs.unlinkSync(imagePath);
+//       }
+
+//       const { filename } = req.file;
+//       const imageUrl = `/uploads/${filename}`;
+//       data.imageUrl = imageUrl;
+//     }
+
+//     data.text = text;
+//     await data.save();
+
+//     res.json(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server listening on port 3000');
+// });
