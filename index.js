1bda2e14457376e0ac7bce2df5aaf3d7efa911a3
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2023-03-03 13:23:07 +0530

    it wiil

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(cors({ credentials: true, origin: 'https://dulcet-profiterole-3d8138.netlify.app/' }));
+app.use(cors({ credentials: true, origin: 'https://dulcet-profiterole-3d8138.netlify.app' }));
 
 
 const port = 4000;
