1ebf49c445c90b6b0fba77d59a97ba16a0200818
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2023-03-24 02:06:32 +0530

    qwert

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.static('public'));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // app.use(cors({ credentials: true, origin: 'http://192.168.1.48:3000' }));
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-// app.use(cors({ credentials: true, origin: 'https://invoice-generater.netlify.app' }));
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://invoice-generater.netlify.app' }));
 // app.use(cors({ credentials: true, origin: 'http://192.168.43.98:3000' }));
 const port = 4000;
 
